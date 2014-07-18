835001c764eb4d5acfa58861b55fb5265c2f3ac0
Woody Chung <wc9825@gmail.com>
2014-07-18 10:47:22 -0700

    Changed color. I like it

---
+++ app/assets/javascripts/format.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+    $('body').css('display', 'none');
+
+    $('body').fadeIn(1000);
+
+    $('a.link').click(function(event) {
+        event.preventDefault();
+        newLocation = this.href;
+        $('body').fadeOut(1000, newpage);
+    });
+    function newpage() {
+        window.location = newLocation;
+    }
+
+});
