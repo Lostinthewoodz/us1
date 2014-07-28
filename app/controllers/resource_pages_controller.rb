d06dc934bba5cd41566783d49b8dfb5eb545fe01
Woody Chung <wc9825@gmail.com>
2014-07-28 10:54:59 -0700

    Added menu to agent resources

--- app/controllers/resource_pages_controller.rb
+++ app/controllers/resource_pages_controller.rb
@@ -1,7 +1,17 @@
 class ResourcePagesController < ApplicationController
+  before_action :signed_in_user, only: [:agents]
+
   def agents
   end
 
   def travelers
   end
+
+  private 
+  	def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
 end
