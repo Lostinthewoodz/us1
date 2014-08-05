71d7042cbcb8563c4be9a2bb83a27d055ee3f70b
Woody Chung <wc9825@gmail.com>
2014-08-05 12:52:30 -0700

    changed default

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "app27788021@heroku.com"
   def welcome_email(user)
   	@user = user
   	mail(to: @user.email, subject: 'test welcome | email')
