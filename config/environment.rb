a0a6a33331fd337d72d6864b43f92a6d39c2617b
Woody Chung <wc9825@gmail.com>
2014-08-05 12:47:38 -0700

    changed env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Us1::Application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name      => ENV['app27788021@heroku.com'],
+  :password       => ENV['0ofoexey'],
+  :domain => "heroku.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
