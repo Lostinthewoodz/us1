d2ffbc947dab77ca49399454b40092934a651e91
Woody Chung <wc9825@gmail.com>
2014-08-05 14:10:43 -0700

    chagned email form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default from: "contact@us1travel.com"
+  default from: "support@us1travel.com"
   def contact_email(contact)
   	@contact = contact
   	mail(to: "wc9825@gmail.com", subject: @contact.title)
