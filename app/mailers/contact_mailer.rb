f772a8e0d739a63984402ac9f20a77b6baac40a5
Woody Chung <wc9825@gmail.com>
2014-08-05 14:10:32 -0700

    changed email form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ActionMailer::Base
   default from: "contact@us1travel.com"
   def contact_email(contact)
   	@contact = contact
-  	mail(to: "wc9825@gmail.com", subject: 'test welcome | email')
+  	mail(to: "wc9825@gmail.com", subject: @contact.title)
   end
 end
