472577f11b0379d3682703607f51d2747a93c79c
Woody Chung <wc9825@gmail.com>
2014-08-05 14:14:50 -0700

    changed once again

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ActionMailer::Base
   default from: "support@us1travel.com"
   def contact_email(contact)
   	@contact = contact
-  	mail(to: "wc9825@gmail.com", subject: @contact.title)
+  	mail(to: "support@us1travel.com", subject: @contact.title)
   end
 end
