4961510107f9f14e110f868de4d862902baf3fce
Woody Chung <wc9825@gmail.com>
2014-08-08 13:43:06 -0700

    awefawe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :show, :feed]
   before_action :correct_user,   only: []
-  before_action :admin_user,     only: [:destroy, :index, :edit, :update]
+  before_action :admin_user,     only: [:destroy, :index, :new, :edit, :update]
 
 
   def show
