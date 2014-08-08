abcf3395ccee8bec5286ed294f978c8d869f3716
Woody Chung <wc9825@gmail.com>
2014-08-08 12:44:09 -0700

    add saber and nongds: success!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation, :admin)
+                                   :password_confirmation, :admin, :nongds, :sabre)
     end
 
     # Before filters
